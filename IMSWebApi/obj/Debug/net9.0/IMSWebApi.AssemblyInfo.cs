cfb3cc5670c08621c5fa819275890cd0f9623725
nsunendar <sunendarnendi@gmail.com>
2025-08-25 07:05:25 +0700

    #Update2

--- IMSWebApi/obj/Debug/net9.0/IMSWebApi.AssemblyInfo.cs
+++ IMSWebApi/obj/Debug/net9.0/IMSWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMSWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef505dd72bf0d56072551c56953d4cda486933f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMSWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMSWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IMSWebApp/obj/Debug/net9.0/IMSWebApp.AssemblyInfo.cs
+++ IMSWebApp/obj/Debug/net9.0/IMSWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMSWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef505dd72bf0d56072551c56953d4cda486933f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMSWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMSWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
